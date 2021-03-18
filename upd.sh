0a20824265b041a1ee9ecf6d15003e4a96231b05
Reini Urban <rurban@cpan.org>
2021-03-18 06:33:08 +0100

    pod: renamed perl11.github.com repo to perl11.github.io
    this is the real name. and more important since perl11.org is gone

--- upd.sh
+++ upd.sh
@@ -4,8 +4,8 @@ hugo
 #broken with 0.20, fixed in 0.59
 #sed -i 's,"/page,"/blog/page,' public/*.html
 
-echo /usr/src/perl/blead/perl11.github.com/blog/
-cd /usr/src/perl/blead/perl11.github.com/blog/
+echo /usr/src/perl/blead/perl11.github.io/blog/
+cd /usr/src/perl/blead/perl11.github.io/blog/
 rsync -avz ~/Documents/perl11-blog/public/* .
 git status
-echo "cd /usr/src/perl/blead/perl11.github.com/; git status"
+echo "cd /usr/src/perl/blead/perl11.github.io/; git status"
